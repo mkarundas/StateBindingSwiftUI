e921da7b5657c369e5e306162a1af2ac18682d14
Arundas MK <mkarundas@gmail.com>
2024-04-20 11:29:29 +0530

    initial commit

---
+++ StateBinding/StateBinding/StateBindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateBindingApp.swift
+//  StateBinding
+//
+//  Created by Arundas MK on 20/04/24.
+//
+
+import SwiftUI
+
+@main
+struct StateBindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
